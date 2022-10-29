dbb3293ea9a23599bf8940670ecfb80bff788819
Renat Suleymanov <mail@renat.su>
2022-10-29 19:08:52 +0400

    fix & refactor initial commit

--- mainform.pas
+++ mainform.pas
@@ -6,111 +6,58 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, EditBtn, StdCtrls, ExtCtrls, Grids, ComCtrls, SynEdit,
-  SynHighlighterXML;
+  Laz2_DOM
+  ;
 
 type
 
-  { TForm1 }
+  { TFrmMain }
 
-  TForm1 = class(TForm)
-    Button1: TButton;
-    FileNameEdit1: TFileNameEdit;
-    Label1: TLabel;
-    StringGrid1: TStringGrid;
-    ToolBar1: TToolBar;
-    procedure Button1Click(Sender: TObject);
-    procedure Button2Click(Sender: TObject);
+  TFrmMain = class(TForm)
+    Btn: TButton;
+    FlNmEdt: TFileNameEdit;
+    StrngGrd: TStringGrid;
+    TlBr: TToolBar;
+    procedure BtnClick({%H-}Sender: TObject);
   private
-
+    function ParseFromOneTable(var aTableNode: TDOMNode; var aRow: Integer): Boolean;
   public
 
   end;
 
 var
-  Form1: TForm1;
+  FrmMain: TFrmMain;
 
 implementation
 
 uses
-  Laz2_DOM, laz2_XMLRead, laz2_xmlutils, StrUtils, eventlog
+  laz2_XMLRead, laz2_xmlutils, StrUtils, eventlog
   ;
 
 {$R *.lfm}
 
-{ TForm1 }
+{ TFrmMain }
 
-procedure TForm1.Button1Click(Sender: TObject);
+procedure TFrmMain.BtnClick(Sender: TObject);
 var
   aDoc: TXMLDocument;
   aContentNode, aNode: TDOMNode;
   aLogger: TEventLog;
   aRow: Integer;
-
-  function ParseFromOneTable(aTableNode: TDOMNode): Boolean;
-  var
-    aArticle, aName, aPV, aBV,aPPrice, aCPrice: String; 
-    aNodeCell, aNodeRow: TDOMNode;
-    i: Integer;
-  begin
-    Result:=False;
-    if not aTableNode.HasChildNodes then
-      Exit;
-    aNodeRow:=aTableNode.FirstChild;
-    i:=0;
-    while Assigned(aNodeRow) do
-    begin
-      while not aNodeRow.HasChildNodes do
-        aNodeRow:=aNodeRow.NextSibling;
-      if i>-1 then
-      begin
-        aNodeCell:=aNodeRow.FirstChild;
-        aArticle:=Trim(aNodeCell.TextContent);
-        aNodeCell:=aNodeCell.NextSibling;
-        aName:=Trim(DelSpace1(aNodeCell.TextContent));
-        aNodeCell:=aNodeCell.NextSibling;
-        aPV:=Trim(aNodeCell.TextContent);
-        aNodeCell:=aNodeCell.NextSibling;
-        aBV:=Trim(aNodeCell.TextContent);
-        aNodeCell:=aNodeCell.NextSibling;
-        aNodeCell:=aNodeCell.NextSibling;
-        aPPrice:=Trim(aNodeCell.TextContent);
-        aNodeCell:=aNodeCell.NextSibling;
-        aNodeCell:=aNodeCell.NextSibling;
-        aCPrice:=Trim(aNodeCell.TextContent);
-
-        Inc(aRow);
-        StringGrid1.InsertRowWithValues(aRow, [aArticle, aName, aPV, aBV, aPPrice, aCPrice]);
-        //aLogger.Info(aNodeRow.Attributes.GetNamedItem('table:style-name').NodeValue);
-      end;
-      Inc(i);
-      aNodeRow:=aNodeRow.NextSibling;
-    end;
-    Result:=True;
-  end;
-
 begin
   aLogger:=TEventLog.Create(nil);
   aLogger.LogType:=ltFile;
   aLogger.Info('Log started');
-  ReadXMLFile(aDoc, FileNameEdit1.FileName);
+  ReadXMLFile(aDoc, FlNmEdt.FileName);
   try  
     aRow:=0;
     aContentNode:=aDoc.FindNode('office:document').FindNode('office:body').FindNode('office:text');
     aNode:=aContentNode.FindNode('table:table');
     if not Assigned(aNode) or not aNode.HasChildNodes then
       Exit;
-    while not SameStr(aNode.NodeName, 'table:table') do
-      aNode:=aNode.NextSibling;
-    if aRow=0 then
-      aNode:=aNode.NextSibling;// first table is the header of a visual table
-    ParseFromOneTable(aNode);       
-    aLogger.Info(aNode.TextContent);
-    aNode:=aNode.NextSibling;
-    aLogger.Info(aNode.NamespaceURI);
-    aLogger.Info(aNode.TextContent);
-    while not SameStr(aNode.NodeName, 'table:table') do
-      aNode:=aNode.NextSibling;
-    ParseFromOneTable(aNode);
+    ParseFromOneTable(aNode, aRow);
+    ParseFromOneTable(aNode, aRow);  
+    ParseFromOneTable(aNode, aRow);
   finally
     aLogger.Info('Log finished');
     aDoc.Free;
@@ -118,9 +65,42 @@ begin
   end;
 end;
 
-procedure TForm1.Button2Click(Sender: TObject);
+function TFrmMain.ParseFromOneTable(var aTableNode: TDOMNode; var aRow: Integer): Boolean;
+var
+  aArticle, aName, aPV, aBV,aPPrice, aCPrice: String;
+  aNodeCell, aNodeRow: TDOMNode;
 begin
+  Result:=False;
+  aTableNode:=aTableNode.NextSibling;// first table is the header of a visual table
+  while not SameStr(aTableNode.NodeName, 'table:table') do
+    aTableNode:=aTableNode.NextSibling;
+  if not aTableNode.HasChildNodes then
+    Exit;
+  aNodeRow:=aTableNode.FirstChild;
+  while Assigned(aNodeRow) do
+  begin
+    while not aNodeRow.HasChildNodes do
+      aNodeRow:=aNodeRow.NextSibling;
+    aNodeCell:=aNodeRow.FirstChild;
+    aArticle:=Trim(aNodeCell.TextContent);
+    aNodeCell:=aNodeCell.NextSibling;
+    aName:=Trim(DelSpace1(aNodeCell.TextContent));
+    aNodeCell:=aNodeCell.NextSibling;
+    aPV:=Trim(aNodeCell.TextContent);
+    aNodeCell:=aNodeCell.NextSibling;
+    aBV:=Trim(aNodeCell.TextContent);
+    aNodeCell:=aNodeCell.NextSibling;
+    aNodeCell:=aNodeCell.NextSibling;
+    aPPrice:=Trim(aNodeCell.TextContent);
+    aNodeCell:=aNodeCell.NextSibling;
+    aNodeCell:=aNodeCell.NextSibling;
+    aCPrice:=Trim(aNodeCell.TextContent);
 
+    Inc(aRow);
+    StrngGrd.InsertRowWithValues(aRow, [aArticle, aName, aPV, aBV, aPPrice, aCPrice]);
+    aNodeRow:=aNodeRow.NextSibling;
+  end;
+  Result:=True;
 end;
 
 end.
