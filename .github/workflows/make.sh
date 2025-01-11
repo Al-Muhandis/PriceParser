6f1f6a3968c04b7ebd39aeadd92f5519d3d2e139
Artem V. Ageev <artem.v.ageev@gmail.com>
2025-01-11 16:38:26 +0200

    fix github-actions

---
+++ .github/workflows/make.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+##############################################################################################################
+
+function priv_clippit
+(
+    cat <<EOF
+https://google.github.io/styleguide/shellguide.html
+https://guide.bash.academy
+https://devhints.io/bash
+https://tldr.sh
+
+Usage: bash ${0} [OPTIONS]
+Options:
+    build   Build program
+EOF
+)
+
+function priv_lazbuild
+(
+    printf '\x1b[33m%s\x1b[0m\n' 'LOAD MAKE.JSON'
+    mapfile -t <"${0//sh/json}"
+    declare -A VAR
+    while read -r; do
+        VAR[${REPLY}]=$(jq --raw-output --exit-status ".${REPLY}" <<< "${MAPFILE[@]}")
+    done < <(jq --raw-output --exit-status 'keys.[]' <<< "${MAPFILE[@]}")
+    declare -rA VAR
+    printf '\x1b[33m%s\x1b[0m\n' 'CHECK PATH WITH .LPI'
+    if ! [[ -d "${VAR[app]}" ]]; then
+        printf '\x1b[32m\t%s\x1b[0m\n' "${VAR[app]} did not find!"
+        exit 1
+    fi >&2
+    printf '\x1b[33m%s\x1b[0m\n' 'UPDATE GIT SUBMODULE'
+    if [[ -f '.gitmodules' ]]; then
+        git submodule update --init --recursive --force --remote &
+    fi
+    printf '\x1b[33m%s\x1b[0m\n' 'INSTALL LAZBUILD'
+    if ! (command -v lazbuild); then
+        # shellcheck source=/dev/null
+        source '/etc/os-release'
+        case ${ID:?} in
+            debian | ubuntu)
+                sudo apt-get update
+                sudo apt-get install -y lazarus{-ide-qt5,} &
+                ;;
+        esac
+    fi &>/dev/null
+    wait
+    printf '\x1b[33m%s\x1b[0m\n' 'LOAD PACKAGES FROM OPM'
+    while read -r; do
+        (
+            declare -rA TMP=(
+                [url]="https://packages.lazarus-ide.org/${REPLY}.zip"
+                [dir]="${HOME}/.lazarus/onlinepackagemanager/packages/${REPLY}"
+                [out]=$(mktemp)
+            )
+            if ! [[ -d "${TMP[dir]}" ]] &&
+               ! (lazbuild --verbose-pkgsearch "${REPLY}") &&
+               ! (lazbuild --add-package "${REPLY}"); then
+                    wget --quiet --output-document "${TMP[out]}" "${TMP[url]}"
+                    mkdir --parents "${TMP[dir]}"
+                    unzip -o "${TMP[out]}" -d "${TMP[dir]}"
+                    rm --verbose "${TMP[out]}"
+                    find "${TMP[dir]}" -type 'f' -name '*.lpk' -printf '\033[33m\tadd package link\t%p\033[0m\n' -exec \
+                        lazbuild --add-package-link {} + >&2
+            fi
+        ) &
+    done < <(jq --raw-output --exit-status '.pkg[]' <<< "${MAPFILE[@]}")
+    wait
+    if [[ -d "${VAR[lib]}" ]]; then
+        printf '\x1b[33m%s\x1b[0m\n' 'LOAD OTHER .LPK'
+        while read -r; do
+            if ! [[ ${REPLY} =~ (cocoa|gdi|_template) ]]; then
+                lazbuild --add-package-link "${REPLY}"
+                printf '\x1b[32m\t%s\x1b[0m\n' "${REPLY} add package link"
+            fi
+        done < <(find "${VAR[lib]}" -type 'f' -name '*.lpk')
+    fi >&2
+    declare -i exitCode=0
+    if [[ -f "${VAR[tst]}" ]]; then
+        printf '\x1b[33m%s\x1b[0m\n' 'RUN UNITTEST'
+        read -r < <(
+            lazbuild --build-all --recursive --no-write-project "${VAR[tst]}" |
+                awk '/Linking/{print $3}'
+        )
+        if ! ("${REPLY}" --all --format=plain --progress >&2); then
+            ((exitCode+=1))
+        fi
+    fi
+    printf '\x1b[33m%s\x1b[0m\n' 'BUILD APPLICATIONS'
+    while read -r; do
+        mapfile -t < <(mktemp)
+        if (lazbuild --build-all --recursive --no-write-project "${REPLY:?}" > "${MAPFILE[0]:?}"); then
+            printf '\x1b[32m\t%s\x1b[0m\n' "[${?}] ${REPLY:?}"
+            grep --color='always' 'Linking' "${MAPFILE[0]:?}"
+        else
+            printf '\x1b[31m\t%s\x1b[0m\n' "[${?}] ${REPLY:?}"
+            grep --color='always' --extended-regexp '(Error|Fatal):' "${MAPFILE[0]:?}"
+            ((exitCode+=1))
+        fi >&2
+        rm "${MAPFILE[0]:?}"
+    done < <(find "${VAR[app]}" -type 'f' -name '*.lpi')
+    exit "${exitCode}"
+)
+
+function priv_main
+(
+    set -euo pipefail
+    if ((${#})); then
+        case ${1} in
+            build) priv_lazbuild ;;
+            *) priv_clippit ;;
+        esac
+    else
+        priv_clippit
+    fi
+)
+
+##############################################################################################################
+priv_main "${@}" >/dev/null
